7bdf437a87d3e5515c73ba719fe151fde2ba33ef
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-08-01 19:06:41 -0700

    Fix the :timeout option to subscribe [#18]
    send_command catches Qrack::ClientTimeout, in order to support
    @read_write_timeout. But ClientTimeout can be raised for another reason:
    To support the :timeout option in Qrack::Subscription#start. That
    gets mishandled, so using the :timeout option can result in the connection
    getting closed.
    
    This separates out the two kinds of timeout. :socket_timeout continues to
    use ClientTimeout. But the :timeout option now uses FrameTimeout
    (as it indicates a timeout in next_frame).

--- lib/bunny/client.rb
+++ lib/bunny/client.rb
@@ -123,7 +123,7 @@ module Bunny
       when channel.frame_buffer.size > 0
         frame = channel.frame_buffer.shift
       when (timeout = opts[:timeout]) && timeout > 0
-        Bunny::Timer::timeout(timeout, Qrack::ClientTimeout) do
+        Bunny::Timer::timeout(timeout, Qrack::FrameTimeout) do
           frame = Qrack::Transport::Frame.parse(buffer)
         end
       else
