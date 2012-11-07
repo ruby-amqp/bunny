7639281a93e24fa8f70e2e3559db9ebe148cc57f
Michael S. Klishin <michael@novemberain.com>
2012-11-07 23:18:59 +0400

    More examples for exchange.declare

--- spec/higher_level_api/integration/exchange_declare_spec.rb
+++ spec/higher_level_api/integration/exchange_declare_spec.rb
@@ -18,17 +18,57 @@ describe Bunny::Exchange do
         x    = ch.fanout(name)
         x.name.should == name
 
+        x.delete
         ch.close
       end
     end
   end
 
   context "of type direct" do
+    context "with a non-predefined name" do
+      it "is declared" do
+        ch = connection.create_channel
+        ch.open
+
+        name = "bunny.tests.exchages.direct"
+        x    = ch.direct(name)
+        x.name.should == name
+
+        x.delete
+        ch.close
+      end
+    end
   end
 
   context "of type topic" do
+    context "with a non-predefined name" do
+      it "is declared" do
+        ch = connection.create_channel
+        ch.open
+
+        name = "bunny.tests.exchages.topic"
+        x    = ch.topic(name)
+        x.name.should == name
+
+        x.delete
+        ch.close
+      end
+    end
   end
 
   context "of type headers" do
+    context "with a non-predefined name" do
+      it "is declared" do
+        ch = connection.create_channel
+        ch.open
+
+        name = "bunny.tests.exchages.headers"
+        x    = ch.headers(name)
+        x.name.should == name
+
+        x.delete
+        ch.close
+      end
+    end
   end
 end
