952bbdeac73ae118388474f2d66f6976e9e761d0
Michael S. Klishin <michael@novemberain.com>
2012-11-08 02:27:18 +0400

    Two more examples for queue.unbind

--- spec/higher_level_api/integration/queue_bind_spec.rb
+++ spec/higher_level_api/integration/queue_bind_spec.rb
@@ -32,10 +32,23 @@ describe "A client-named", Bunny::Queue do
     ch = connection.create_channel
     q  = ch.queue("bunny.tests.queues.client-named#{rand}", :exclusive => true)
 
-    x  = ch.fanout("bunny.tests.exchanges.fanout1")
+    x  = ch.fanout("bunny.tests.exchanges.fanout#{rand}")
     q.bind(x).should be_instance_of(AMQ::Protocol::Queue::BindOk)
 
     x.delete
+    ch.close
+  end
+
+  it "can be unbound from a custom exchange" do
+    ch = connection.create_channel
+    q  = ch.queue("bunny.tests.queues.client-named#{rand}", :exclusive => true)
+    q.should_not be_server_named
+
+    x  = ch.fanout("bunny.tests.fanout", :auto_delete => true, :durable => false)
+
+    q.bind(x)
+    q.unbind(x).should be_instance_of(AMQ::Protocol::Queue::UnbindOk)
+
     ch.close
   end
 end
@@ -74,8 +87,21 @@ describe "A server-named", Bunny::Queue do
     ch = connection.create_channel
     q  = ch.queue("", :exclusive => true)
 
-    x  = ch.fanout("bunny.tests.exchanges.fanout2")
+    x  = ch.fanout("bunny.tests.exchanges.fanout#{rand}")
     q.bind(x).should be_instance_of(AMQ::Protocol::Queue::BindOk)
+
+    x.delete
+    ch.close
+  end
+
+  it "can be bound from a custom exchange" do
+    ch   = connection.create_channel
+    q    = ch.queue("", :exclusive => true)
+
+    name = "bunny.tests.exchanges.fanout#{rand}"
+    x    = ch.fanout(name)
+    q.bind(x)
+    q.unbind(name).should be_instance_of(AMQ::Protocol::Queue::UnbindOk)
 
     x.delete
     ch.close
