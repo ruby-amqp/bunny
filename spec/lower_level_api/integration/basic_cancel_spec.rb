b0566e24a2a9af926c68fbceff2706a2b7ef758b
Michael Klishin <michael@defprotocol.org>
2012-11-24 21:55:47 +0400

    Basic coverage for basic.cancel

--- spec/lower_level_api/integration/basic_cancel_spec.rb
+++ spec/lower_level_api/integration/basic_cancel_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe Bunny::Channel, "#basic_consume" do
+describe Bunny::Channel, "#basic_cancel" do
   let(:connection) do
     c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed")
     c.start
@@ -25,7 +25,30 @@ describe Bunny::Channel, "#basic_consume" do
   end
 
   context "when the given consumer tag is valid" do
-    it "cancels the consumer"
+    let(:queue_name) { "bunny.basic.cancel.queue#{rand}" }
+
+    it "cancels the consumer" do
+      delivered_data = []
+
+      t = Thread.new do
+        ch         = connection.create_channel
+        q          = ch.queue(queue_name, :auto_delete => true, :durable => false)
+        consume_ok = ch.basic_consume(q, "", true, false) do |metadata, payload|
+          delivered_data << payload
+        end
+
+        consume_ok.consumer_tag.should_not be_nil
+        cancel_ok = ch.basic_cancel(consume_ok.consumer_tag)
+        cancel_ok.consumer_tag.should == consume_ok.consumer_tag
+
+        ch.close
+      end
+      t.abort_on_exception = true
+      sleep 0.5
+
+      sleep 0.7
+      delivered_data.should be_empty
+    end
   end
 
   context "when the given consumer tag is invalid (was never registered)" do
