ff9ee84a9abb01b9f9253ba6706432eb29a68b1e
Michael Klishin <michael@defprotocol.org>
2012-11-29 19:55:30 +0400

    First example for the new doc site

---
+++ examples/guides/getting_started/hello_world.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "rubygems"
+require "bunny"
+
+conn = Bunny.new
+conn.start
+
+ch = conn.create_channel
+q  = ch.queue("bunny.examples.hello_world", :auto_delete => true)
+x  = ch.default_exchange
+
+q.subscribe do |metadata, payload|
+  puts "Received #{payload}"
+end
+
+x.publish("Hello!", :routing_key => q.name)
+
+sleep 1.0
+conn.close
