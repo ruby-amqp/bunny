8ca067536405f84f0934428a18e02aa1e1359496
Michael Klishin <michael@defprotocol.org>
2013-01-14 21:22:01 +0400

    Code example that demonstrates handling of channel-level exceptions
    [ci skip]

---
+++ examples/connection/channel_level_exception.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bundler"
+Bundler.setup
+
+$:.unshift(File.expand_path("../../../lib", __FILE__))
+
+require 'bunny'
+
+conn = Bunny.new(:heartbeat_interval => 8)
+conn.start
+
+begin
+  ch1 = conn.create_channel
+  ch1.queue_delete("queue_that_should_not_exist#{rand}")
+rescue Bunny::NotFound => e
+  puts "Channel-level exception! Code: #{e.channel_close.reply_code}, message: #{e.channel_close.reply_text}"
+end
+
+
+begin
+  ch2 = conn.create_channel
+  q   = "bunny.examples.recovery.q#{rand}"
+
+  ch2.queue_declare(q, :durable => false)
+  ch2.queue_declare(q, :durable => true)
+rescue Bunny::PreconditionFailed => e
+  puts "Channel-level exception! Code: #{e.channel_close.reply_code}, message: #{e.channel_close.reply_text}"
+ensure
+  conn.create_channel.queue_delete(q)
+end
+
+puts "Disconnecting..."
+conn.close
