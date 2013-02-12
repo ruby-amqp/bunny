43617e7905e116a2018a0298d6ae4e33650d0732
Michael Klishin <michael@defprotocol.org>
2013-02-12 13:01:05 +0400

    Rearrange specs, add new examples for #97

---
+++ spec/issues/issue97_spec.rb
@@ -0,0 +1,155 @@
+require "spec_helper"
+
+describe "Message framing implementation" do
+  let(:connection) do
+    c = Bunny.new(:user     => "bunny_gem",
+                  :password => "bunny_password",
+                  :vhost    => "bunny_testbed",
+                  :port     => ENV.fetch("RABBITMQ_PORT", 5672))
+    c.start
+    c
+  end
+
+  after :all do
+    connection.close if connection.open?
+  end
+
+
+  context "with payload exceeding 128 Kb (max frame size)" do
+    it "successfully frames the message" do
+      ch = connection.create_channel
+
+      q  = ch.queue("", :exclusive => true)
+      x  = ch.default_exchange
+
+      as = ("a" * (1024 * 1024 * 4 + 28237777))
+      x.publish(as, :routing_key => q.name, :persistent => true)
+
+      sleep(1)
+      q.message_count.should == 1
+
+      _, _, payload      = q.pop
+      payload.bytesize.should == as.bytesize
+
+      ch.close
+    end
+  end
+
+
+
+  context "with empty message body" do
+    it "successfully publishes the message" do
+      ch = connection.create_channel
+
+      q  = ch.queue("", :exclusive => true)
+      x  = ch.default_exchange
+
+      x.publish("", :routing_key => q.name, :persistent => true)
+
+      sleep(1)
+      q.message_count.should == 1
+
+      envelope, headers, payload = q.pop
+
+      payload.should == ""
+
+      headers[:content_type].should == "application/octet-stream"
+      headers[:delivery_mode].should == 2
+      headers[:priority].should == 0
+
+      ch.close
+    end
+  end
+
+
+  context "with payload being 2 bytes less than 128K bytes in size" do
+    it "successfully frames the message" do
+      ch = connection.create_channel
+
+      q  = ch.queue("", :exclusive => true)
+      x  = ch.default_exchange
+
+      as = "a" * (1024 * 128 - 2)
+      x.publish(as, :routing_key => q.name, :persistent => true)
+
+      sleep(1)
+      q.message_count.should == 1
+
+      q.purge
+      ch.close
+    end
+  end
+
+  context "with payload being 1 byte less than 128K bytes in size" do
+    it "successfully frames the message" do
+      ch = connection.create_channel
+
+      q  = ch.queue("", :exclusive => true)
+      x  = ch.default_exchange
+
+      as = "a" * (1024 * 128 - 1)
+      x.publish(as, :routing_key => q.name, :persistent => true)
+
+      sleep(1)
+      q.message_count.should == 1
+
+      q.purge
+      ch.close
+    end
+  end
+
+  context "with payload being exactly 128K bytes in size" do
+    it "successfully frames the message" do
+      ch = connection.create_channel
+
+      q  = ch.queue("", :exclusive => true)
+      x  = ch.default_exchange
+
+      as = "a" * (1024 * 128)
+      x.publish(as, :routing_key => q.name, :persistent => true)
+
+      sleep(1)
+      q.message_count.should == 1
+
+      q.purge
+      ch.close
+    end
+  end
+
+
+  context "with payload being 1 byte greater than 128K bytes in size" do
+    it "successfully frames the message" do
+      ch = connection.create_channel
+
+      q  = ch.queue("", :exclusive => true)
+      x  = ch.default_exchange
+
+      as = "a" * (1024 * 128 + 1)
+      x.publish(as, :routing_key => q.name, :persistent => true)
+
+      sleep(1)
+      q.message_count.should == 1
+
+      q.purge
+      ch.close
+    end
+  end
+
+  context "with payload being 2 bytes greater than 128K bytes in size" do
+    it "successfully frames the message" do
+      ch = connection.create_channel
+
+      q  = ch.queue("", :exclusive => true)
+      x  = ch.default_exchange
+
+      as = "a" * (1024 * 128 + 2)
+      x.publish(as, :routing_key => q.name, :persistent => true)
+
+      sleep(1)
+      q.message_count.should == 1
+
+      q.purge
+      ch.close
+    end
+  end
+end
