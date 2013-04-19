17382a34b6c50d0ceb80ef6976d7574cbfc3aa10
Michael Klishin <michael@defprotocol.org>
2013-04-19 07:23:33 +0400

    A few more test cases with single-threaded connections

--- spec/higher_level_api/integration/basic_publish_spec.rb
+++ spec/higher_level_api/integration/basic_publish_spec.rb
@@ -14,6 +14,7 @@ describe "Publishing a message to the default exchange" do
 
   context "with all default delivery and message properties" do
     it "routes messages to a queue with the same name as the routing key" do
+      connection.should be_threaded
       ch = connection.create_channel
 
       q  = ch.queue("", :exclusive => true)
@@ -34,6 +35,7 @@ describe "Publishing a message to the default exchange" do
 
   context "with all default delivery and message properties" do
     it "routes the messages and preserves all the metadata" do
+      connection.should be_threaded
       ch = connection.create_channel
 
       q  = ch.queue("", :exclusive => true)
@@ -51,6 +53,33 @@ describe "Publishing a message to the default exchange" do
       headers[:content_type].should == "application/octet-stream"
       headers[:delivery_mode].should == 2
       headers[:priority].should == 0
+
+      ch.close
+    end
+  end
+
+
+  context "with all default delivery and message properties on a single-threaded connection" do
+    let(:connection) do
+      c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed", :threaded => false)
+      c.start
+      c
+    end
+
+    it "routes messages to a queue with the same name as the routing key" do
+      connection.should_not be_threaded
+      ch = connection.create_channel
+
+      q  = ch.queue("", :exclusive => true)
+      x  = ch.default_exchange
+
+      x.publish("xyzzy", :routing_key => q.name).
+        publish("xyzzy", :routing_key => q.name).
+        publish("xyzzy", :routing_key => q.name).
+        publish("xyzzy", :routing_key => q.name)
+
+      sleep(1)
+      q.message_count.should == 4
 
       ch.close
     end
