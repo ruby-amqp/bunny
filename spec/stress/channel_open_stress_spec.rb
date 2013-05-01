162f69cf3c24d735b2ceb398ed838fe00c1864df
Michael Klishin <michael@defprotocol.org>
2013-05-01 19:07:37 +0400

    Exclude stress tests from CI

--- spec/stress/channel_open_stress_spec.rb
+++ spec/stress/channel_open_stress_spec.rb
@@ -1,48 +1,50 @@
 require "spec_helper"
 
-describe "Rapidly opening and closing lots of channels" do
-  let(:connection) do
-    c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed", :automatic_recovery => false)
-    c.start
-    c
-  end
-
-  after :all do
-    connection.close
-  end
-
-  context "in a single-threaded scenario" do
-    let(:n) { 500 }
-
-    it "works correctly" do
-      xs = Array.new(n) { connection.create_channel }
-      puts "Opened #{n} channels"
-
-      xs.size.should == n
-      xs.each do |ch|
-        ch.close
-      end
+unless ENV["CI"]
+  describe "Rapidly opening and closing lots of channels" do
+    let(:connection) do
+      c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed", :automatic_recovery => false)
+      c.start
+      c
     end
-  end
 
-  context "in a multi-threaded scenario" do
-    # actually, on MRI values greater than ~100 will eventually cause write
-    # operations to fail with a timeout (1 second is not enough)
-    # which will cause recovery to re-acquire @channel_mutex in Session.
-    # Because Ruby's mutexes are not re-entrant, it will raise a ThreadError.
-    #
-    # But this already demonstrates that within these platform constraints,
-    # Bunny is safe to use in such scenarios.
-    let(:n) { 50 }
+    after :all do
+      connection.close
+    end
 
-    it "works correctly" do
-      n.times do
-        t = Thread.new do
-          ch = connection.create_channel
+    context "in a single-threaded scenario" do
+      let(:n) { 500 }
 
+      it "works correctly" do
+        xs = Array.new(n) { connection.create_channel }
+        puts "Opened #{n} channels"
+
+        xs.size.should == n
+        xs.each do |ch|
           ch.close
         end
-        t.abort_on_exception = true
+      end
+    end
+
+    context "in a multi-threaded scenario" do
+      # actually, on MRI values greater than ~100 will eventually cause write
+      # operations to fail with a timeout (1 second is not enough)
+      # which will cause recovery to re-acquire @channel_mutex in Session.
+      # Because Ruby's mutexes are not re-entrant, it will raise a ThreadError.
+      #
+      # But this already demonstrates that within these platform constraints,
+      # Bunny is safe to use in such scenarios.
+      let(:n) { 50 }
+
+      it "works correctly" do
+        n.times do
+          t = Thread.new do
+            ch = connection.create_channel
+
+            ch.close
+          end
+          t.abort_on_exception = true
+        end
       end
     end
   end
--- spec/stress/concurrent_publishers_stress_spec.rb
+++ spec/stress/concurrent_publishers_stress_spec.rb
@@ -1,58 +1,60 @@
 # -*- coding: utf-8 -*-
 require "spec_helper"
 
-describe "Concurrent publishers sharing a connection" do
-  let(:connection) do
-    c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed", :automatically_recover => false)
-    c.start
-    c
-  end
-
-  after :all do
-    connection.close
-  end
-
-  let(:n) { 32 }
-  let(:m) { 1000 }
-
-  it "successfully finish publishing" do
-    ch = connection.create_channel
-
-    q    = ch.queue("", :exclusive => true)
-    body = "сообщение"
-
-    # let the queue name be sent back by RabbitMQ
-    sleep 0.25
-
-    chs  = {}
-    n.times do |i|
-      chs[i] = connection.create_channel
+unless ENV["CI"]
+  describe "Concurrent publishers sharing a connection" do
+    let(:connection) do
+      c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed", :automatically_recover => false)
+      c.start
+      c
     end
 
-    ts = []
+    after :all do
+      connection.close
+    end
 
-    n.times do |i|
-      t = Thread.new do
-        cht = chs[i]
-        x   = ch.default_exchange
+    let(:n) { 32 }
+    let(:m) { 1000 }
 
-        5.times do |i|
-          m.times do
-            x.publish(body, :routing_key => q.name)
-          end
-          puts "Published #{(i + 1) * m} messages..."
-        end
+    it "successfully finish publishing" do
+      ch = connection.create_channel
+
+      q    = ch.queue("", :exclusive => true)
+      body = "сообщение"
+
+      # let the queue name be sent back by RabbitMQ
+      sleep 0.25
+
+      chs  = {}
+      n.times do |i|
+        chs[i] = connection.create_channel
       end
-      t.abort_on_exception = true
 
-      ts << t
+      ts = []
+
+      n.times do |i|
+        t = Thread.new do
+          cht = chs[i]
+          x   = ch.default_exchange
+
+          5.times do |i|
+            m.times do
+              x.publish(body, :routing_key => q.name)
+            end
+            puts "Published #{(i + 1) * m} messages..."
+          end
+        end
+        t.abort_on_exception = true
+
+        ts << t
+      end
+
+      ts.each do |t|
+        t.join
+      end
+
+      sleep 4.0
+      q.message_count.should == 5 * n * m
     end
-
-    ts.each do |t|
-      t.join
-    end
-
-    sleep 4.0
-    q.message_count.should == 5 * n * m
   end
 end
