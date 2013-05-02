c1054c6660a1d2488a29cb3e8c0dd3a31fa116ef
Michael Klishin <michael@defprotocol.org>
2013-05-02 14:10:06 +0400

    Exclude more stress tests from CI runs

--- spec/issues/issue100_spec.rb
+++ spec/issues/issue100_spec.rb
@@ -1,40 +1,42 @@
 require "spec_helper"
 
-describe Bunny::Channel, "#basic_publish" do
-  let(:connection) do
-    c = Bunny.new(:user     => "bunny_gem",
-                  :password => "bunny_password",
-                  :vhost    => "bunny_testbed",
-                  :socket_timeout => 0)
-    c.start
-    c
-  end
+unless ENV["CI"]
+  describe Bunny::Channel, "#basic_publish" do
+    let(:connection) do
+      c = Bunny.new(:user     => "bunny_gem",
+                    :password => "bunny_password",
+                    :vhost    => "bunny_testbed",
+                    :socket_timeout => 0)
+      c.start
+      c
+    end
 
-  after :all do
-    connection.close if connection.open?
-  end
+    after :all do
+      connection.close if connection.open?
+    end
 
 
-  context "when publishing thousands of messages" do
-    let(:n) { 10_000 }
-    let(:m) { 10 }
+    context "when publishing thousands of messages" do
+      let(:n) { 10_000 }
+      let(:m) { 10 }
 
-    it "successfully publishers them all" do
-      ch = connection.create_channel
+      it "successfully publishers them all" do
+        ch = connection.create_channel
 
-      q  = ch.queue("", :exclusive => true)
-      x  = ch.default_exchange
+        q  = ch.queue("", :exclusive => true)
+        x  = ch.default_exchange
 
-      body = "x" * 1024
-      m.times do |i|
-        n.times do
-          x.publish(body, :routing_key => q.name)
+        body = "x" * 1024
+        m.times do |i|
+          n.times do
+            x.publish(body, :routing_key => q.name)
+          end
+          puts "Published #{i * n} messages so far..."
         end
-        puts "Published #{i * n} messages so far..."
-      end
 
-      q.purge
-      ch.close
+        q.purge
+        ch.close
+      end
     end
   end
 end
