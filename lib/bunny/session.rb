a16abea9548742fa0245c58b265383b59ce41261
Michael Klishin <michael@defprotocol.org>
2013-05-03 16:02:01 +0400

    Log heartbeat interval used as INFO

--- lib/bunny/session.rb
+++ lib/bunny/session.rb
@@ -700,6 +700,7 @@ module Bunny
                                 negotiate_value(@client_heartbeat, connection_tune.heartbeat)
                               end
       @logger.debug "Heartbeat interval negotiation: client = #{@client_heartbeat}, server = #{connection_tune.heartbeat}, result = #{@heartbeat}"
+      @logger.info "Heartbeat interval used (in seconds): #{@heartbeat}"
 
       @channel_id_allocator = ChannelIdAllocator.new(@channel_max)
 
