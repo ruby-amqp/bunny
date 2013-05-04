6d78190f4c581cee0682df327601f0f2dd6c5b11
Michael Klishin <michael@defprotocol.org>
2013-05-04 21:12:40 +0400

    Introduce Bunny::Socket#write_nonblock_fully

--- lib/bunny/socket.rb
+++ lib/bunny/socket.rb
@@ -20,6 +20,13 @@ module Bunny
       end
     end
 
+    # Reads given number of bytes with an optional timeout
+    #
+    # @param [Integer] count How many bytes to read
+    # @param [Integer] timeout Timeout
+    #
+    # @return [String] Data read from the socket
+    # @api public
     def read_fully(count, timeout = nil)
       return nil if @__bunny_socket_eof_flag__
 
@@ -40,6 +47,36 @@ module Bunny
         end
       end
       value
+    end # read_fully
+
+    # Writes provided data using IO#write_nonblock, taking care of handling
+    # of exceptions it raises when writing would fail (e.g. due to socket buffer
+    # being full).
+    #
+    # IMPORTANT: this method will mutate (slice) the argument. Pass in duplicates
+    # if this is not appropriate in your case.
+    #
+    # @param [String] data Data to write
+    #
+    # @api public
+    def write_nonblock_fully(data)
+      return nil if @__bunny_socket_eof_flag__
+      if data.bytesize <= 4096
+        self.write_nonblock(data)
+        return data.bytesize
+      end
+
+      begin
+        while !data.empty?
+          written = self.write_nonblock(data)
+          data.slice!(0, written)
+        end
+      rescue Errno::EWOULDBLOCK, Errno::EAGAIN
+        IO.select([], [self])
+        retry
+      end
+
+      data.bytesize
     end
   end
 end
