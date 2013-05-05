24eac772b855c70f30c26efb1284d171a735394d
Michael Klishin <michael@defprotocol.org>
2013-05-05 15:34:49 +0400

    Adapt this stress test to be more likely reproduce #119

--- spec/stress/concurrent_publishers_stress_spec.rb
+++ spec/stress/concurrent_publishers_stress_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 unless ENV["CI"]
   describe "Concurrent publishers sharing a connection" do
     let(:connection) do
-      c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed", :automatically_recover => false)
+      c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed", :automatically_recover => false, :continuation_timeout => 20.0)
       c.start
       c
     end
@@ -13,8 +13,8 @@ unless ENV["CI"]
       connection.close
     end
 
-    let(:n) { 32 }
-    let(:m) { 1000 }
+    let(:concurrency) { 24 }
+    let(:rate)        { 5_000 }
 
     it "successfully finish publishing" do
       ch = connection.create_channel
@@ -26,22 +26,22 @@ unless ENV["CI"]
       sleep 0.25
 
       chs  = {}
-      n.times do |i|
+      concurrency.times do |i|
         chs[i] = connection.create_channel
       end
 
       ts = []
 
-      n.times do |i|
+      concurrency.times do |i|
         t = Thread.new do
           cht = chs[i]
           x   = ch.default_exchange
 
           5.times do |i|
-            m.times do
+            rate.times do
               x.publish(body, :routing_key => q.name)
             end
-            puts "Published #{(i + 1) * m} messages..."
+            puts "Published #{(i + 1) * rate} messages..."
           end
         end
         t.abort_on_exception = true
@@ -52,9 +52,6 @@ unless ENV["CI"]
       ts.each do |t|
         t.join
       end
-
-      sleep 4.0
-      q.message_count.should == 5 * n * m
     end
   end
 end
