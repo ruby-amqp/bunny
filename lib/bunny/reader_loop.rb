431027f8969a69760f882fc59d2a2a6f5b4e549c
Michael Klishin <michael@defprotocol.org>
2013-06-07 13:51:37 +0400

    Exclude impl details from API reference

--- lib/bunny/reader_loop.rb
+++ lib/bunny/reader_loop.rb
@@ -6,6 +6,7 @@ module Bunny
   # This loop uses a separate thread internally.
   #
   # This mimics the way RabbitMQ Java is designed quite closely.
+  # @private
   class ReaderLoop
 
     def initialize(transport, session, session_thread)
