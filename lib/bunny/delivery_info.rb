cc154608aa410e46d58dc20ec8a2612a4bcac494
Michael Klishin <michael@defprotocol.org>
2013-06-07 13:44:26 +0400

    Reference API for Bunny::DeliveryInfo

--- lib/bunny/delivery_info.rb
+++ lib/bunny/delivery_info.rb
@@ -1,5 +1,5 @@
 module Bunny
-  # Wraps AMQ::Protocol::Basic::Deliver to
+  # Wraps {AMQ::Protocol::Basic::Deliver} to
   # provide access to the delivery properties as immutable hash as
   # well as methods.
   class DeliveryInfo
@@ -14,8 +14,12 @@ module Bunny
     # API
     #
 
-    attr_reader :consumer, :channel
+    # @return [Bunny::Consumer] Consumer this delivery is for
+    attr_reader :consumer
+    # @return [Bunny::Channel] Channel this delivery is on
+    attr_reader :channel
 
+    # @private
     def initialize(basic_deliver, consumer, channel)
       @basic_deliver = basic_deliver
       @hash          = {
@@ -31,43 +35,55 @@ module Bunny
       @channel       = channel
     end
 
+    # Iterates over the delivery properties
+    # @see Enumerable#each
     def each(*args, &block)
       @hash.each(*args, &block)
     end
 
+    # Accesses delivery properties
+    # @see Hash#[]
     def [](k)
       @hash[k]
     end
 
+    # @return [Hash] Hash representation of this delivery info
     def to_hash
       @hash
     end
 
+    # @private
     def to_s
       to_hash.to_s
     end
 
+    # @private
     def inspect
       to_hash.inspect
     end
 
+    # @return [String] Consumer tag this delivery is for
     def consumer_tag
       @basic_deliver.consumer_tag
     end
 
+    # @return [String] Delivery identifier that is used to acknowledge, reject and nack deliveries
     def delivery_tag
       @basic_deliver.delivery_tag
     end
 
+    # @return [Boolean] true if this delivery is a redelivery (the message was requeued at least once)
     def redelivered
       @basic_deliver.redelivered
     end
     alias redelivered? redelivered
 
+    # @return [String] Name of the exchange this message was published to
     def exchange
       @basic_deliver.exchange
     end
 
+    # @return [String] Routing key this message was published with
     def routing_key
       @basic_deliver.routing_key
     end
