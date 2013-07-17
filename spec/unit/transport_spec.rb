a2386dae88f6d19bd30640a657110ea90bc70212
Michael Klishin <michael@defprotocol.org>
2013-07-17 22:50:11 +0400

    Use a host that does not try to block incoming traffic in this test

--- spec/unit/transport_spec.rb
+++ spec/unit/transport_spec.rb
@@ -3,12 +3,12 @@ require "spec_helper"
 # mystically fails on Rubinius on CI. MK.
 if RUBY_ENGINE != "rbx"
   describe Bunny::Transport, ".reachable?" do
-    it "returns true for google.com, 80" do
-      Bunny::Transport.reacheable?("google.com", 80, 1).should be_true
+    it "returns true for rabbitmq.com, 80" do
+      Bunny::Transport.reacheable?("rabbitmq.com", 80, 1).should be_true
     end
 
-    it "returns true for google.com, 8088" do
-      Bunny::Transport.reacheable?("google.com", 8088, 1).should be_false
+    it "returns true for rabbitmq.com, 18088" do
+      Bunny::Transport.reacheable?("rabbitmq.com", 18088, 1).should be_false
     end
 
     it "returns false for google1237982792837.com, 8277" do
