ecfc09c47097e05d42589459ad40e53d547f9bf2
Michael Klishin <michael@defprotocol.org>
2013-07-25 22:41:54 +0400

    Eliminate a race condition in Bunny::Session#close

--- spec/higher_level_api/integration/connection_stop_spec.rb
+++ spec/higher_level_api/integration/connection_stop_spec.rb
@@ -1,12 +1,15 @@
 require "spec_helper"
 
 describe Bunny::Session do
-  it "can be closed" do
-    c  = Bunny.new
-    c.start
-    ch = c.create_channel
+  4000.times do |i|
+    it "can be closed (take #{i})" do
+      c  = Bunny.new(:automatically_recover => false)
+      c.start
+      ch = c.create_channel
 
-    c.should be_connected
-    c.stop
+      c.should be_connected
+      c.stop
+      c.should be_closed
+    end
   end
 end
