627174dde50f36091f8dace4ec5cffdbbbcf311d
Michael Klishin <michael@defprotocol.org>
2013-07-26 09:30:27 +0400

    Move this to stress tests
    Excluded on JRuby because aggressive thread creation and shutdown
    in a loop is really slow.

---
+++ spec/stress/connection_open_close_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+unless RUBY_ENGINE == "jruby"
+  describe Bunny::Session do
+    4000.times do |i|
+      it "can be closed (take #{i})" do
+        c  = Bunny.new(:automatically_recover => false)
+        c.start
+        ch = c.create_channel
+
+        c.should be_connected
+        c.stop
+        c.should be_closed
+      end
+    end
+  end
+end
