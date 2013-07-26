41bff3cc73995e5aa978eaa620428cfb6d0c900c
Michael Klishin <michael@defprotocol.org>
2013-07-26 09:30:43 +0400

    Make this a "regular" functional test

--- spec/higher_level_api/integration/connection_stop_spec.rb
+++ spec/higher_level_api/integration/connection_stop_spec.rb
@@ -1,21 +1,13 @@
 require "spec_helper"
 
 describe Bunny::Session do
-  n = if RUBY_ENGINE == "jruby"
-        100
-      else
-        4000
-      end
+  it "can be closed" do
+    c  = Bunny.new(:automatically_recover => false)
+    c.start
+    ch = c.create_channel
 
-  n.times do |i|
-    it "can be closed (take #{i})" do
-      c  = Bunny.new(:automatically_recover => false)
-      c.start
-      ch = c.create_channel
-
-      c.should be_connected
-      c.stop
-      c.should be_closed
-    end
+    c.should be_connected
+    c.stop
+    c.should be_closed
   end
 end
