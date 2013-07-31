0172cad026b6965a604671dba720efd38de25dd6
Michael Klishin <michael@defprotocol.org>
2013-07-31 15:58:37 +0400

    Demonstrate basic.ack with automatic acknowledgement mode

--- spec/higher_level_api/integration/basic_ack_spec.rb
+++ spec/higher_level_api/integration/basic_ack_spec.rb
@@ -7,46 +7,64 @@ describe Bunny::Channel, "#ack" do
     c
   end
 
-  after :all do
+  after :each do
     connection.close if connection.open?
-  end
-
-  subject do
-    connection.create_channel
   end
 
   context "with a valid (known) delivery tag" do
     it "acknowleges a message" do
-      q = subject.queue("bunny.basic.ack.manual-acks", :exclusive => true)
-      x = subject.default_exchange
+      ch = connection.create_channel
+      q  = ch.queue("bunny.basic.ack.manual-acks", :exclusive => true)
+      x  = ch.default_exchange
 
       x.publish("bunneth", :routing_key => q.name)
       sleep(0.25)
       q.message_count.should == 1
       delivery_details, properties, content = q.pop(:ack => true)
 
-      subject.ack(delivery_details.delivery_tag, true)
+      ch.ack(delivery_details.delivery_tag, true)
       sleep(0.25)
       q.message_count.should == 0
 
-      subject.close
+      ch.close
+    end
+  end
+
+
+  context "with a valid (known) delivery tag and automatic ack mode" do
+    it "acknowleges a message" do
+      ch = connection.create_channel
+      q  = ch.queue("bunny.basic.ack.manual-acks", :exclusive => true)
+      x  = ch.default_exchange
+
+      q.subscribe(:manual_ack => false) do |delivery_info, properties, payload|
+        ch.ack(delivery_details.delivery_tag, false)
+      end
+      sleep(0.25)
+
+      x.publish("bunneth", :routing_key => q.name)
+      sleep(0.5)
+      q.message_count.should == 0
+
+      ch.close
     end
   end
 
   context "with an invalid (random) delivery tag" do
     it "causes a channel-level error" do
-      q = subject.queue("bunny.basic.ack.unknown-delivery-tag", :exclusive => true)
-      x = subject.default_exchange
+      ch = connection.create_channel
+      q  = ch.queue("bunny.basic.ack.unknown-delivery-tag", :exclusive => true)
+      x  = ch.default_exchange
 
       x.publish("bunneth", :routing_key => q.name)
       sleep(0.25)
       q.message_count.should == 1
       _, _, content = q.pop(:ack => true)
 
-      subject.on_error do |ch, channel_close|
+      ch.on_error do |ch, channel_close|
         @channel_close = channel_close
       end
-      subject.ack(82, true)
+      ch.ack(82, true)
 
       sleep 0.5
 
