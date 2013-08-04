cca4bdde95638c8051e611dd21c5fbfb4efb3f10
Michael Klishin <michael@defprotocol.org>
2013-08-04 13:32:14 +0400

    Hey arsespec, do what I want

--- spec/stress/connection_open_close_spec.rb
+++ spec/stress/connection_open_close_spec.rb
@@ -2,16 +2,14 @@ require "spec_helper"
 
 unless RUBY_ENGINE == "jruby" && !ENV["FORCE_JRUBY_RUN"]
   describe Bunny::Session do
-    let(:n) do
-      # creating thousands of connections means creating
-      # twice as many threads and this won't fly with the JVM
-      # in CI containers. MK.
-      if RUBY_ENGINE == "jruby"
-        100
-      else
-        5000
-      end
-    end
+    # creating thousands of connections means creating
+    # twice as many threads and this won't fly with the JVM
+    # in CI containers. MK.
+    n = if RUBY_ENGINE == "jruby"
+          100
+        else
+          5000
+        end
 
     n.times do |i|
       it "can be closed (take #{i})" do
