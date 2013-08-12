0d25e11c29dd3cb81875c2ba1c867dd4d756918d
Michael Klishin <michael@defprotocol.org>
2013-08-12 23:19:45 +0400

    Demonstrate that exclusive queues are deleted when *connection* is closed
    Just to make sure I don't confuse the two ever again.

---
+++ spec/higher_level_api/integration/exclusive_queue_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe Bunny::Queue do
+  let(:connection) do
+    c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed")
+    c.start
+    c
+  end
+
+  after :each do
+    connection.close if connection.open?
+  end
+
+  it "is closed when the connection it was declared on is closed" do
+    ch1 = connection.create_channel
+    ch2 = connection.create_channel
+    q   = ch1.queue("", :exclusive => true)
+
+    ch1.queue_declare(q.name, :passive => true)
+    ch2.queue_declare(q.name, :passive => true)
+
+    ch1.close
+    ch2.queue_declare(q.name, :passive => true)
+
+    ch2.queue_delete(q.name)
+    ch2.close
+  end
+end
