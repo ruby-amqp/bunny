d92a1a8ec30eaf2f3488632509e1f455b01aa380
Michael Klishin <michael@clojurewerkz.org>
2013-10-04 02:01:21 +0400

    Guard this test on Rubies > 1.8

--- spec/unit/system_timer_spec.rb
+++ spec/unit/system_timer_spec.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
-describe Bunny::SystemTimer do
-  it 'supports being called with a single argument' do
-    lambda {Bunny::SystemTimer::timeout(1) {}}.should_not raise_error
-    lambda {Bunny::SystemTimer::timeout(1, nil) {}}.should_not raise_error
+if RUBY_VERSION.to_f < 1.9
+  describe Bunny::SystemTimer do
+    it 'supports being called with a single argument' do
+      lambda {Bunny::SystemTimer::timeout(1) {}}.should_not raise_error
+      lambda {Bunny::SystemTimer::timeout(1, nil) {}}.should_not raise_error
+    end
   end
 end
-
