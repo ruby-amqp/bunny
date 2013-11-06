008afecbd8986a3caab40a018beb33fff7ed004b
Michael Klishin <mklishin@gopivotal.com>
2013-11-06 20:44:53 +0400

    Separate Bunny::Socket and Bunny::SSLSocket implementations for CRuby and JRuby
    Avoids CPU burn on JRuby because of excessive EWOULDBLOCK exceptions being raised.
    
    References #165, #166.

---
+++ lib/bunny/jruby/socket.rb
@@ -0,0 +1,40 @@
+require "bunny/cruby/socket"
+
+module Bunny
+  module JRuby
+    # TCP socket extension that uses Socket#readpartial to avoid excessive CPU
+    # burn after some time. See issue #165.
+    # @private
+    class Socket < Bunny::Socket
+
+      # Reads given number of bytes with an optional timeout
+      #
+      # @param [Integer] count How many bytes to read
+      # @param [Integer] timeout Timeout
+      #
+      # @return [String] Data read from the socket
+      # @api public
+      def read_fully(count, timeout = nil)
+        return nil if @__bunny_socket_eof_flag__
+
+        value = ''
+        begin
+          loop do
+            value << readpartial(count - value.bytesize)
+            break if value.bytesize >= count
+          end
+        rescue EOFError
+          # @eof will break Rubinius' TCPSocket implementation. MK.
+          @__bunny_socket_eof_flag__ = true
+        rescue *READ_RETRY_EXCEPTION_CLASSES
+          if IO.select([self], nil, nil, timeout)
+            retry
+          else
+            raise Timeout::Error, "IO timeout when reading #{count} bytes"
+          end
+        end
+        value
+      end # read_fully
+    end
+  end
+end
