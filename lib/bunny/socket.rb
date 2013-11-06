f59c4b904b77217eb284e0762b383bab9f1446c3
Michael Klishin <mklishin@gopivotal.com>
2013-11-06 22:48:05 +0400

    Facepalm

--- lib/bunny/socket.rb
+++ lib/bunny/socket.rb
@@ -3,7 +3,7 @@ if defined?(JRUBY_VERSION)
   require "bunny/jruby/socket"
 
   module Bunny
-    SocketImpl = Socket #JRuby::Socket
+    SocketImpl = JRuby::Socket
   end
 else
   require "bunny/cruby/socket"
