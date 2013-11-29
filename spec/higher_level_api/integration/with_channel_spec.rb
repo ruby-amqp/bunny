032d48ffcfb55c336e07cc1dbc1a4d2cdb2e02fd
Carl Hörberg <carl.hoerberg@gmail.com>
2013-11-29 10:46:32 +0700

    Ensure that we close with_channel after exception

---
+++ spec/higher_level_api/integration/with_channel_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe Bunny::Channel, "#with_channel" do
+  let(:connection) do
+    c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed")
+    c.start
+    c
+  end
+
+  after :each do
+    connection.close if connection.open?
+  end
+
+  it "closes if the block throws an exception" do
+    ch = nil
+    begin
+      connection.with_channel do |wch|
+        ch = wch
+        raise Exception.new
+      end
+    rescue Exception
+    end
+    ch.should be_closed
+  end
+end
