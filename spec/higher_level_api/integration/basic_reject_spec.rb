833c681bd3913632507834bf4d1bd51f6f0d360d
Wayne Conrad <wayne@databill.com>
2015-06-04 10:04:50 -0700

    Added tests for Channel#basic_reject

--- spec/higher_level_api/integration/basic_reject_spec.rb
+++ spec/higher_level_api/integration/basic_reject_spec.rb
@@ -75,3 +75,56 @@ describe Bunny::Channel, "#reject" do
     end
   end
 end
+
+describe Bunny::Channel, "#basic_reject" do
+  let(:connection) do
+    c = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed")
+    c.start
+    c
+  end
+
+  after :each do
+    connection.close if connection.open?
+  end
+
+  context "with requeue = true" do
+    it "requeues a message" do
+      ch = connection.create_channel
+      q  = ch.queue("bunny.basic.reject.manual-acks", :exclusive => true)
+      x  = ch.default_exchange
+
+      x.publish("bunneth", :routing_key => q.name)
+      sleep(0.5)
+      expect(q.message_count).to eq 1
+      delivery_info, _, _ = q.pop(:manual_ack => true)
+
+      ch.basic_reject(delivery_info.delivery_tag.to_i, true)
+      sleep(0.5)
+      expect(q.message_count).to eq 1
+
+      ch.close
+    end
+  end
+
+  context "with requeue = false" do
+    it "rejects a message" do
+      ch = connection.create_channel
+      q  = ch.queue("bunny.basic.reject.with-requeue-false", :exclusive => true)
+      x  = ch.default_exchange
+
+      x.publish("bunneth", :routing_key => q.name)
+      sleep(0.5)
+      expect(q.message_count).to eq 1
+      delivery_info, _, _ = q.pop(:manual_ack => true)
+
+      ch.basic_reject(delivery_info.delivery_tag.to_i, false)
+      sleep(0.5)
+      ch.close
+
+      ch = connection.create_channel
+      q  = ch.queue("bunny.basic.reject.with-requeue-false", :exclusive => true)
+      expect(q.message_count).to eq 0
+      ch.close
+    end
+  end
+end
