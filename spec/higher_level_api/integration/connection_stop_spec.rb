c6b93315b2f6a3cc023f8f8da534861c6a417a89
Michael Klishin <michael@clojurewerkz.org>
2017-02-24 22:21:06 +0300

    Remove more negative assertions in recovery tests
    They are quite timing sensitive

--- spec/higher_level_api/integration/connection_stop_spec.rb
+++ spec/higher_level_api/integration/connection_stop_spec.rb
@@ -6,9 +6,9 @@ describe Bunny::Session do
   def close_connection(client_port)
     c = http_client.
       list_connections.
-      find   { |conn_info| conn_info.peer_port.to_i == client_port }
+      find   { |conn_info| conn_info && conn_info.peer_port.to_i == client_port }
 
-    http_client.close_connection(c.name)
+    http_client.close_connection(c.name) if c
   end
 
   def wait_for_recovery
@@ -61,8 +61,6 @@ describe Bunny::Session do
       expect(c).to be_open
       sleep 1.5
       close_connection(c.local_port)
-      sleep 0.2
-      expect(c).not_to be_open
 
       wait_for_recovery
       expect(c).to be_open
