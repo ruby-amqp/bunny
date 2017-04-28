97c1192afb74408ec2ce7a624409a412f9123631
Alessandro Verlato <averlato@gmail.com>
2017-04-28 13:54:11 +0200

    Fix #491

--- spec/issues/issue491_spec.rb
+++ spec/issues/issue491_spec.rb
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 include RabbitMQ::Control
 
 describe Bunny::Session do
-  let(:logger) { @logz ||= Logger.new STDOUT }
+  let(:logger) { @lgr ||= Logger.new STDOUT }
 
   let(:connection) do
     Bunny.new(
@@ -16,58 +15,48 @@ describe Bunny::Session do
     )
   end
 
-  it 'has a weird behaviour when the broker goes away, then returns but connection cannot complete' do
-    # Start RabbitMQ
-    Thread.new { RabbitMQ::Control.start_rabbitmq }
+  context 'after connection recovery is triggered' do
+    def start_rabbitmq
+      # Start RabbitMQ
+      Thread.new { RabbitMQ::Control.start_rabbitmq }
 
-    # Ensure Rabbitmq has almost started
-    loop do
-      break if RabbitMQ::Control.rabbitmq_pid
-      sleep 1
-    end
-
-    # Give broker some additional time to startup
-    logger.debug ' --> Giving RabbiMQ time to startup'
-    sleep 5
-
-    # Connect to RabbitMQ
-    connection.start
-    logger.debug ' ---> Connected to broker'
-    channel = connection.create_channel
-    def_exchange = channel.default_exchange
-
-    # Redefine Bunny::Session#send_preamble
-    $_count = 0
-    connection.define_singleton_method(:send_preamble) do
-      logger.debug ' --->  Inside redefined :send_preamble'
-      @transport.write(AMQ::Protocol::PREAMBLE)
-      sleep 6 if $_count == 0  # At least 5 seconds sleep on my machine
-      @logger.debug 'Sent protocol preamble'
-      $_count += 1
-    end
-
-    # Start a thread that publishes 1 message/sec
-    t = Thread.new do
-      50.times do |n|
-        puts "Publishing message ##{n}"
-        def_exchange.publish 'Hello, World!', routing_key: 'whatever'
+      # Ensure Rabbitmq has almost started
+      loop do
         sleep 1
+        break if RabbitMQ::Control.rabbitmq_pid
+      end
+
+      # Give broker some additional time to startup
+      sleep 5
+    end
+
+
+    describe '#recover_from_network_failure' do
+      before :each do
+        start_rabbitmq
+        connection.start
+      end
+
+      after :each do
+        start_rabbitmq
+      end
+
+      it 'should try to recover after a SystemCallError exception is raised during a reconnection attempt' do
+
+        # Redefine Session#send_preamble so that it triggers a SystemCallError
+        connection.define_singleton_method(:send_preamble) do
+          @transport.write(AMQ::Protocol::PREAMBLE)
+          @logger.debug 'Sent protocol preamble redefined'
+          raise SystemCallError
+        end
+
+        # Kill the broker
+        RabbitMQ::Control.kill_rabbitmq
+
+        expect(connection).to receive(:announce_network_failure_recovery)
+
+        sleep 6
       end
     end
-
-    # Kill the broker
-    logger.debug ' --->  Killing RabbitMQ'
-    RabbitMQ::Control.kill_rabbitmq
-
-    # Give it some time to detect connection failure and begin connection recovery ceremony
-    sleep 6
-
-
-    # Restart the broker
-    logger.debug ' --->  Starting RabbitMQ'
-    RabbitMQ::Control.start_rabbitmq
-
-    # Wait for publishing thread
-    t.join
   end
 end
