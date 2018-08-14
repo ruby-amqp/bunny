492c335ad37a64ff4d883360bd735ec28b3652df
Scott Bonebrake <scott.bonebrake@microsoft.com>
2018-08-14 16:51:46 -0700

    Add first toxiproxy test case

---
+++ spec/higher_level_api/toxiproxy/basic_publish_toxi_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+require_relative "./toxiproxy_helper"
+
+describe Bunny::Channel, "#basic_publish" do
+  before(:all) do
+    setup_toxiproxy
+    @connection = Bunny.new(:user => "bunny_gem", :password => "bunny_password", :vhost => "bunny_testbed",
+      host: "localhost:11111", heartbeat_timeout: 1)
+    @connection.start
+  end
+
+  after :all do
+    @connection.close if @connection.open?
+  end
+
+  context "when the the connection isn't open" do
+    let(:queue_name) { "bunny.basic.publish.queue#{rand}" }
+
+    it "raises a ConnectionClosedError" do
+      ch = @connection.create_channel
+      begin
+        rabbitmq_toxiproxy.down do
+          sleep 2
+          expect { ch.default_exchange.publish("", :routing_key => queue_name) }.to raise_error(Bunny::ConnectionClosedError)
+        end
+      ensure
+        cleanup_toxiproxy
+      end
+    end
+  end
+end
---
+++ spec/higher_level_api/toxiproxy/toxiproxy_helper.rb
@@ -0,0 +1,16 @@
+def setup_toxiproxy
+  Toxiproxy.populate([{
+    name: "rabbitmq",
+    listen: "0.0.0.0:11111",
+    upstream: "rabbitmq:5672"
+  }])
+  rabbitmq_toxiproxy.enable
+end
+
+def cleanup_toxiproxy
+  Toxiproxy.populate()
+end
+
+def rabbitmq_toxiproxy
+  Toxiproxy[/rabbitmq/]
+end
