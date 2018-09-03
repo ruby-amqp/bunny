22716809e5dbecfae7f7a0de2ca78e37bdaf98c5
Michael Klishin <michael@clojurewerkz.org>
2018-09-04 01:18:29 +0200

    Check the LOCAL_RABBITMQ env var when determining Toxiproxy backend hostname

--- spec/toxiproxy_helper.rb
+++ spec/toxiproxy_helper.rb
@@ -1,10 +1,18 @@
 module RabbitMQ
   module Toxiproxy
+    RABBITMQ_UPSTREAM_HOST = if !ENV["LOCAL_RABBITMQ"].nil?
+                               # a local Toxiproxy/RabbitMQ combination
+                               "localhost"
+                             else
+                               # docker-compose
+                               "rabbitmq"
+                             end
+
     def setup_toxiproxy
       ::Toxiproxy.populate([{
         name: "rabbitmq",
         listen: "0.0.0.0:11111",
-        upstream: "rabbitmq:5672"
+            upstream: "#{RABBITMQ_UPSTREAM_HOST}:5672"
       }])
       rabbitmq_toxiproxy.enable
     end
