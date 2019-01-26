0297983f1fd89aac981272e4e3078a79d00deb4d
Michael Klishin <michael@clojurewerkz.org>
2019-01-26 16:32:12 +0300

    Update Docker Compose bits
     * Provision latest RabbitMQ and Erlang from Bintray via apt
     * Use a new style RabbitMQ configuration file
     * Update docker-compose.yml to a newer format
     * Use rabbitmqctl shutdown instead of sleeping
     * Edits to CONTRIBUTING.md (mention Toxiproxy, etc)

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,35 +3,24 @@ server=rabbitmq-server
 ctl=rabbitmqctl
 delay=5
 
-echo "[Configuration] $(eval cat $RABBITMQ_CONFIG_FILE.config)"
-
-echo '[Configuration] Starting RabbitMQ in detached mode.'
-
+echo 'Starting a RabbitMQ node'
 $server -detached
 
-echo "[Configuration] Waiting $delay seconds for RabbitMQ to start."
+echo "Waiting $delay seconds for RabbitMQ to start."
 
 sleep $delay
 
-echo '*** Creating users ***'
 $ctl add_user bunny_gem bunny_password
 $ctl add_user bunny_reader reader_password
 
-echo '*** Creating virtual hosts ***'
 $ctl add_vhost bunny_testbed
 
-echo '*** Setting virtual host permissions ***'
 $ctl set_permissions -p / guest '.*' '.*' '.*'
 $ctl set_permissions -p bunny_testbed bunny_gem '.*' '.*' '.*'
 $ctl set_permissions -p bunny_testbed guest '.*' '.*' '.*'
 $ctl set_permissions -p bunny_testbed bunny_reader '^---$' '^---$' '.*'
 
-$ctl stop
+$ctl shutdown --timeout 10
 
-echo "[Configuration] Waiting $delay seconds for RabbitMQ to stop."
-
-sleep $delay
-
-echo 'Starting RabbitMQ in foreground (CTRL-C to exit)'
-
+echo 'Starting a RabbitMQ node in foreground (use Ctrl-C to stop)'
 exec $server
