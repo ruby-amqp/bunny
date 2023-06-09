549c1b7beb306ce94b8d63c6e659cff21c81a827
Marc Schmitz <marc.schmitz@invision.de>
2023-06-09 11:58:27 +0200

    Notify on connection recovery fail
    It’s possible to specify the recovery_attempts option.
    Hitting the recovery attempts limit without a successful recovery
    leads to a cleanup of connection, etc.
    However, the client is not informed about this state and has no chance
    to react upon it.
    
    Here we introduce a callback which is called in exactly the above
    described situation enabling client code to react to a failed recovery
    process.

--- lib/bunny/session.rb
+++ lib/bunny/session.rb
@@ -128,6 +128,7 @@ module Bunny
     # @option connection_string_or_opts [Integer] :reset_recovery_attempts_after_reconnection (true) Should recovery attempt counter be reset after successful reconnection? When set to false, the attempt counter will last through the entire lifetime of the connection object.
     # @option connection_string_or_opts [Proc] :recovery_attempt_started (nil) Will be called before every connection recovery attempt
     # @option connection_string_or_opts [Proc] :recovery_completed (nil) Will be called after successful connection recovery
+    # @option connection_string_or_opts [Proc] :recovery_failed (nil) Will be called when the connection recovery failed after the specified amount of recovery attempts
     # @option connection_string_or_opts [Boolean] :recover_from_connection_close (true) Should this connection recover after receiving a server-sent connection.close (e.g. connection was force closed)?
     # @option connection_string_or_opts [Object] :session_error_handler (Thread.current) Object which responds to #raise that will act as a session error handler. Defaults to Thread.current, which will raise asynchronous exceptions in the thread that created the session.
     #
@@ -225,6 +226,7 @@ module Bunny
 
       @recovery_attempt_started = opts[:recovery_attempt_started]
       @recovery_completed       = opts[:recovery_completed]
+      @recovery_failed          = opts[:recovery_failed]
 
       @session_error_handler = opts.fetch(:session_error_handler, Thread.current)
 
@@ -553,6 +555,12 @@ module Bunny
       @recovery_completed = block
     end
 
+    # Defines a callable (e.g. a block) that will be called
+    # when the connection recovery failed after the specified
+    # numbers of recovery attempts.
+    def after_recovery_failed(&block)
+      @recovery_failed = block
+    end
 
     #
     # Implementation
@@ -809,6 +817,7 @@ module Bunny
           @transport.close
           self.close(false)
           @manually_closed = false
+          notify_of_recovery_failed
         end
       else
         raise e
@@ -857,6 +866,11 @@ module Bunny
     # @private
     def notify_of_recovery_completion
       @recovery_completed.call if @recovery_completed
+    end
+
+    # @private
+    def notify_of_recovery_failed
+      @recovery_failed.call if @recovery_failed
     end
 
     # @private
