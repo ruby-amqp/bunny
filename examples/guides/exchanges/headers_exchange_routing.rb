015cd52f42453a5435dc7d7eadc9cdc15765ceac
Michael Klishin <michaelklishin@icloud.com>
2025-05-22 19:26:19 -0400

    Modernize a headers exchange routing example rabbitmq/rabbitmq-server#13934

--- examples/guides/exchanges/headers_exchange_routing.rb
+++ examples/guides/exchanges/headers_exchange_routing.rb
@@ -13,8 +13,8 @@ conn.start
 ch   = conn.create_channel
 x    = ch.headers("headers")
 
-q1   = ch.queue("", :exclusive => true).bind(x, :arguments => {"os" => "linux", "cores" => 8, "x-match" => "all"})
-q2   = ch.queue("", :exclusive => true).bind(x, :arguments => {"os" => "osx",   "cores" => 4, "x-match" => "any"})
+q1   = ch.queue("headers.q1.linux.8cores").bind(x, :arguments => {"os" => "linux", "cores" => 8, "x-match" => "all"})
+q2   = ch.queue("headers.q2.macos.any", exclusive: true).bind(x, :arguments => {"os" => "macos",   "cores" => 4, "x-match" => "any"})
 
 q1.subscribe do |delivery_info, properties, content|
   puts "#{q1.name} received #{content}"
@@ -24,7 +24,7 @@ q2.subscribe do |delivery_info, properties, content|
 end
 
 x.publish("8 cores/Linux", :headers => {"os" => "linux", "cores" => 8})
-x.publish("8 cores/OS X",  :headers => {"os" => "osx",   "cores" => 8})
+x.publish("8 cores/OS X",  :headers => {"os" => "macos",   "cores" => 8})
 x.publish("4 cores/Linux", :headers => {"os" => "linux", "cores" => 4})
 
 sleep 0.5
